0586a56b22b5573df5c21525bbe38e03dc742052
g-dave <32879783+g-dave@users.noreply.github.com>
2019-05-13 17:47:17 +0200

    Download TOTP
    added
    guacamole extension folder as well as download and extract one time password extension from guacamole website

--- prepare.sh
+++ prepare.sh
@@ -2,6 +2,11 @@
 #
 echo "Preparing folder init and creating ./init/initdb.sql"
 mkdir ./init >/dev/null 2>&1
+mkdir -p ./guacamole/extensions/
+wget -O ./guacamole/extensions/guacamole-auth-totp-1.0.0.tar.gz "http://apache.org/dyn/closer.cgi?action=download&filename=guacamole/1.0.0/binary/guacamole-auth-totp-1.0.0.tar.gz"
+tar xfvz ./guacamole/extensions/guacamole-auth-totp-1.0.0.tar.gz -C ./guacamole/extensions/
+mv ./guacamole/extensions/guacamole-auth-totp-1.0.0/guacamole-auth-totp-1.0.0.jar ./guacamole/extensions/
 chmod -R +x ./init
 docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > ./init/initdb.sql
+
 echo "done"
